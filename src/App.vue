2ccdf717af70047da275ee9a923fd80361cff8af
Joao <joao.rampoo@gmail.com>
2024-05-03 17:16:24 -0300

    still bug - fix label

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const estado = reactive({
+  tarefas: [
+    {
+      titulo: "Lavar louça",
+      finalizada: false,
+    },
+    {
+      titulo: "Tirar lixo",
+      finalizada: false,
+    },
+    {
+      titulo: "Estudar Vue.JS",
+      finalizada: true,
+    },
+  ],
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="p-5 my-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>Você possui 7 tarefas pendenter</p>
+    </header>
+    <form>
+      <div class="row d-flex justify-content-between">
+        <div class="col">
+          <input
+            type="text"
+            placeholder="Digite aqui a descrição da tarefa"
+            class="form-control"
+          />
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary ms-4">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+          <select class="form-control">
+            <option value="todas">Todas as tarefas</option>
+            <option value="pendentes">Tarefas pendentes</option>
+            <option value="finalizadas">Tarefas finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <ul class="list-group mt-4">
+      <li class="list-group-item" v-for="tarefa in estado.tarefas">
+        <input
+          :checked="tarefa.finalizada"
+          :id="tarefa.titulo"
+          type="checkbox"
+        />
+        <label
+          class="ms-3"
+          :for="tarefa.titulo"
+          :class="{ done: tarefa.finalizada }"
+        >
+          {{ tarefa.titulo }}
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.done {
+  text-decoration: line-through;
 }
 </style>
